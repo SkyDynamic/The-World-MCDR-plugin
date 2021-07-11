ca15e625944611c966526c60df1bde02bbfa8d0a
SkyDynamic <82658163+SkyDynamic@users.noreply.github.com>
2021-07-11 22:02:31 +0800

    Update The World.py

--- The World.py
+++ The World.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import time
-from mcdreforged.api.decorator import new_thread
-from mcdreforged.api.types import ServerInterface, Info
+from mcdreforged.plugin.server_interface import *
+from mcdreforged.api.decorator import *
+from mcdreforged.api.command import *
 
 PERMISSIONS = {
     'help': 1,
@@ -17,34 +18,35 @@ PLUGIN_METADATA = {
     'link': 'https://github.com/SkyDynamic/The-World-MCDR-plugin'
 }
 
-Prefix= '!!Theworld'
+Prefix= '!!TW'
 HelpMessage='''
 §7{0} help §6显示此帮助界面
 §7{0} §6激活时间停止，并且持续5秒
 '''.strip().format(Prefix)
 
-@new_thread('StopTime')
-def stop_time(server: ServerInterface, info: Info)
-    server.execute('title @a title "{}暂停了时间"'.format(info.player))
-    server.execute("effect give @a[name=!{}] minecraft:slowness 1000 255".format(info.player))
-    server.execute("effect give @a[name=!{}] minecraft:blindness 1000 255".format(info.player))
-    server.execute("effect give @a[name=!{}] minecraft:jump_boost 1000 128".format(info.player))
-    server.execute("tick freeze")
+@new_thread
+def stop_time(src: CommandSource):
+    server=src.get_server()
+    server.execute(f'title @a title "§4{src.player}§7暂停了时间"')
+    server.execute(f"effect give @a[name=!{src.player}] minecraft:slowness 1000 255")
+    server.execute(f"effect give @a[name=!{src.player}] minecraft:blindness 1000 255")
+    server.execute(f"effect give @a[name=!{src.player}] minecraft:jump_boost 1000 128")
+    server.execute(f"tick freeze")
     time.sleep( 5 )
-    server.execute("tick freeze")
-    server.execute("effect clear @a")
-    
+    server.execute(f"tick freeze")
+    server.execute(f"effect clear @a")
+
 def on_load(server: ServerInterface, old):
-    server.logger.info(data)
-        server.register_help_message(HelpMessage)
-            server.register_command(
-                Literal('!!Theworld help').
-                    requires(lambda src: src.has_permission(PERMISSIONS['help'])).
-                    then(
-                    src.reply(info,HelpMessage)
-                ).
-                Literal('!!Theworld').
-                    requires(lambda src: src.has_permission(PERMISSIONS['stop time'])).
-                    then(
-                        stop_time(server,info)
-                    )
+    server.register_help_message('!!TW help', '显示The World插件帮助')
+
+    server.register_command(
+        Literal(Prefix).
+            requires(lambda src: src.has_permission(PERMISSIONS['help'])).
+            then(
+            Literal('help').runs(lambda src: src.reply(HelpMessage))
+        ).
+            then(
+                Literal('on').
+                    requires(lambda src: src.has_permission(PERMISSIONS['stop time'])).runs(stop_time)
+        )
+    )
